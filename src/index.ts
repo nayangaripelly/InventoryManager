d2543490930810eecd141958d4f7190b36198258
nayan <garipellysainayan@gmail.com>
2025-07-24 14:30:29 +0530

    implemented signup and login endpts including hashing and jwt signing, used dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,75 @@
 import express from "express";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+const jwtsecret = process.env.JWT_SECRET as string;
+const mongourl = process.env.MONGO_URL as string;
+
+import { userModel,productModel } from "./db";
 const app = express();
 
 app.use(express.json());
 
-app.post("/api/v1/signup",function(req,res)
+app.post("/api/v1/signup",async function(req,res)
 {
+    const {username,password} = req.body;
+    const hash = await bcrypt.hash(password,5);
 
+    try {
+        await userModel.create({
+            username,
+            password:hash
+        });
+
+        res.status(200).json({
+            msg: "successfully signed up !!"
+        })
+    }catch(e)
+    {
+        res.status(500).json({
+            msg : "something went wrong try again"
+        })
+    }
 });
 
-app.post("/api/v1/login",function(req,res)
+app.post("/api/v1/login",async function(req,res)
 {
+    const {username,password} = req.body;
+    try {
+        const user = await userModel.findOne({username});
+        const hash = user?.password as string;
+        if(!user)
+        {
+            res.status(404).json({
+                msg:"user Not found, signup first"
+            });
+            return;
+        }
+        const match = await bcrypt.compare(password, hash);
+        if(!match)
+        {
+            res.status(401).json({
+                msg:"incorrect credentials"
+            })
+            return;
+        }
+        const token = jwt.sign({
+            id:user._id
+        },jwtsecret);
 
+        res.status(200).json({
+            msg:"successfully logged in",
+            token
+        })
+    }catch(e)
+    {
+        res.status(500).json({
+            msg : " something went wrong try again"
+        })
+    }
 });
 
 app.post("/api/v1/products",function(req,res)
@@ -31,7 +89,7 @@ app.get("/api/v1/products",function(req,res)
 
 async function main()
 {
-    mongoose.connect("url");
+    await mongoose.connect(mongourl);
     app.listen(3000);
 }
 
